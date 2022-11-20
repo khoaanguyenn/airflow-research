9f41ba987e342fb4560c41f5f434f7b14f203226
khoaanguyenn <99160953+khoaanguyenn@users.noreply.github.com>
2022-11-21 01:44:50 +0700

    Remove Airflow::Task

--- lib/airflow/job.rb
+++ lib/airflow/job.rb
@@ -15,43 +15,4 @@ module Airflow
       !!@bid
     end
   end
-
-  class Task
-    attr_reader :jid, :channel, :details
-
-    FINAL_STATES = %w[success fail].freeze
-
-    def initialize(channel)
-      @channel = channel
-    end
-
-    FINAL_STATES.each do |state|
-      define_method("#{state}!") do |details = {}|
-        mark!(state, details)
-      end
-    end
-
-    def log(payload)
-      @details = payload
-    end
-
-    # Conclude the task to be either success or failure
-    def concluded?
-      !!@concluded
-    end
-
-    def mark!(status, details = nil)
-      details = details || @details || {}
-      publish(status, details)
-    end
-
-    private
-
-    def publish(status, details)
-      @concluded = true if FINAL_STATES.include?(status)
-      Sidekiq.redis do |conn|
-        conn.publish(@channel, { status: status, details: details.to_json })
-      end
-    end
-  end
 end
